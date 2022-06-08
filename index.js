62d7993be5bd5ab5ffcc96521f71db5ceaac8701
Rahul Kumar Sharma <rahul.sharma@edstem.com>
2022-06-08 08:56:17 +0530

    replace hard coded path name with dynamic one

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const {
 	readFile: readFilePromise
 } = require('fs/promises');
 
+const path = require('path');
+
 const { readFile } = require('fs');
 
-readFile('./files/lorem.txt', 'utf-8', (err, data) => {
+readFile(path.join(__dirname, 'files', 'helloworld.txt'), 'utf-8', (err, data) => {
 	if (err) throw err;
 	console.log(data);
 })
@@ -12,7 +14,8 @@ readFile('./files/lorem.txt', 'utf-8', (err, data) => {
 
 const readFileFromPromise = async () => {
 	try {
-		const response = await readFilePromise('./files/lorem.txt', 'utf-8');
+		const response = await readFilePromise(path
+			.join(__dirname, 'files', 'lorem.txt'), 'utf-8');
 		console.log(response);
 	} catch (error) {
 		console.log(error);
